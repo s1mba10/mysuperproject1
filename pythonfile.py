f363e67a78c63edae2e62ebdc087608c8022a32b
Parallels <parallels@kali-linux-2021-1.localdomain>
2022-03-10 00:04:25 +0300

    last time added

--- pythonfile.py
+++ pythonfile.py
@@ -1,1 +1,7 @@
-print("Hello, world")
+import os
+os.chdir("/home/parallels/Desktop/")
+os.mkdir("/home/parallels/Desktop/created_folder")
+with open("/home/parallels/Desktop/created_folder/created_file.txt", mode="w+") as file:
+    mytext = file.write("Hello, we found vulnerability in your computer \n That is why we're here")
+    file.close()
+
